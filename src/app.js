04eba5014cd426b26c120b7d62f25dc2084cb7ae
Ubuntu <ubuntu@ip-172-31-43-243.ap-south-1.compute.internal>
2023-09-03 12:19:07 +0000

    comments

--- src/app.js
+++ src/app.js
@@ -90,7 +90,7 @@ server.on('stream', (stream, headers) => {
 
 
   if (method === 'GET' && path === '/health') {
-    // console.log("GET REQUEST RECEIVED /health")
+    //console.log("GET REQUEST RECEIVED /health")
     stream.respond({ ':status': 200 });
     return stream.end(JSON.stringify({
       "STATUS": "OK"
